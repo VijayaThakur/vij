51141f3bb0f6b739302a0e014f762158f4d77b22
VijayaThakur <99168500+VijayaThakur@users.noreply.github.com>
2022-04-14 10:20:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+//Variable named "Stripes" to create a new group
+var stripes; createSprite(x, 200, 35, 300); 
+
+function setup() {
+  createCanvas(450, 400);
+   for(var x=30,; x<450,x = x+55)
+
+   //Draw stripes using for loop
+}
+
+function draw() {
+  background("gray");
+  stripe.background=white;
+  drawSprites();
+}
